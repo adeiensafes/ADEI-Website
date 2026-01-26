6d9fdcf9c9252a006de5dda2ff67e3167e3fcec7
moslimar <moslimarabi86@gmail.com>
2026-01-26 23:47:38 +0100

    pages edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import { AuthProvider } from './AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
 import ModernNavbar from './components/ModernNavbar';
